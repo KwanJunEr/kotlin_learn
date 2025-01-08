f6ade786e562be334db72fc28706b59c939863d8
KwanJunEr <jonastheitguy@gmail.com>
2025-01-08 10:39:43 +0800

    completed some basics on section 2 for variable type

--- section2/Kotlin_basics/app/src/main/java/eu/example/kotlinbasics/Basics.kt
+++ section2/Kotlin_basics/app/src/main/java/eu/example/kotlinbasics/Basics.kt
@@ -1,2 +1,16 @@
 package eu.example.kotlinbasics
 
+fun main(){
+
+    //Declaring a type for variable
+    var myAge : Byte = 35;
+    println(myAge);
+    val number1 = 1;
+    var mutableString = "Johan";
+    mutableString = "Robert Johnson";
+
+    println(number1);
+    println(mutableString);
+}
+
+
